ecd50fc8183215b4c9453842e70255f13582cc32
Daniel Rodriguez <drod18@hotmail.com>
2020-09-24 23:40:57 -0500

    Final Submission for completed secure password generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,22 @@
-// Prompts to user narrowing down their password parameters
-
-alert("Hello, in order to assist you we have a few questions for you to answer!");
-
-var includeLowerCase = confirm("Would you like to include lower case letters?");
-var includeUpperCase = confirm("Would you like to include Upper Case letters?");
-var includeNumbers = confirm("Would you like to include numbers?")
-var includeSpecialCharacters = confirm("Would you like to include any special characters?")
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
 
+  // Prompts to user narrowing down their password parameters
+alert("Hello, in order to assist you we have a few questions for you to answer!");
+var length = prompt("Please select the length of your password. It must me between 8 characters and no more tham 128!")
+var lowerCase = confirm("Would you like to include lower case letters?");
+var upperCase = confirm("Would you like to include Upper Case letters?");
+var numbers = confirm("Would you like to include numbers?")
+var specialCharacters = confirm("Would you like to include any special characters?")
+
+// Arrays for special characters, numbers, letters
+var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var specialC = ["~","!","@","#","$","%","&","*",]
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -22,4 +28,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{}
+
+console.log(Math.random)
