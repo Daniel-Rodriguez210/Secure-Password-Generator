8604925a3c4298c7a3c83f19fe28bb21435cd4e4
Daniel Rodriguez <drod18@hotmail.com>
2022-08-20 12:39:30 -0500

    fix the prompt that asks the user for a password length

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Arrays for special characters, numbers, letters
 var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,61 +5,28 @@ var numbericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var selections = []
 
-// Rather than doing a long if/else chain for each scenario for what the user selects I am going to create variables for each upper, lower, number, and special character with booleans set to false and activate them to true if the user states they would like to include each one. 
+// Function to prompt user for password options
 
-function generatePassword() {
-
-  alert("Hello, in order to assist you we have a few questions for you to answer!");
-
-  // This section asks the user for password length and makes sure the user input fits the parameters of the program. 
-
-  var userInput = parseInt(prompt("Please select the length of your password. It must me between 8 characters and no more tham 128!"));
-  if (!userInput) {
-    alert("Item can't be left blank!");
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  )
+   if (isNaN(length) === true) {
+    alert("Password length must be provided as a number");
     return;
-  } else if (userInput < 8 || userInput > 128) {
-    userInput = parseInt(prompt("Your password length must be between 8 and 128."))
+   } 
+   if (length < 8) {
+    alert("Password length must be at least 8 characters");
     return;
   }
-
-  // This section asks the user for what they would like to be included in their password numbers, letters, special characters. 
-  var lowerCase = confirm("Would you like to include lower case letters?");
-  var upperCase = confirm("Would you like to include Upper Case letters?");
-  var numbers = confirm("Would you like to include numbers?");
-  var specialCharacters = confirm("Would you like to include any special characters?");
-
-  if (lowerCase === false && upperCase === false && numbers === false && specialCharacters === false) {
-    alert("You must choose a minimum of 2 to generate a secure password!")
-    return;
-  }
-
-  if (lowerCase) {
-    selections = selections.concat(lowerC);
-  }
-  if (upperCase) { 
-    selections = selections.concat(upperC);
-  }
-  if (numbers) {
-    selections = selections.concat(numbersC);
-  }
-  if (specialCharacters) {
-    selections = selections.concat(specialC);
-    
-  }
-
-
-var password = ""
-  for (i = 0; i < userInput; i++) {
-    password += selections[Math.floor(Math.random() * selections.length)];
-    
-  }
- document.getElementById("password").value = password;
-};
+    if (length > 128) {
+      alert("Password length must be less than 129 characters");
+      return;
+    }
+}
 
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword); { }
 
 
 
